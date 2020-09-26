084f7ee26b26f742965de1892900e444ef0b77b1
AnnaRaven3 <Schmidinger.Anna@sus.htl-grieskirchen.at>
2020-09-26 18:46:04 +0200

    save 4

--- src/net/htl/grieskirchen/pos/aschmidinger18/erstehausuebung2/EratosthenesPrimeSieve.java
+++ src/net/htl/grieskirchen/pos/aschmidinger18/erstehausuebung2/EratosthenesPrimeSieve.java
@@ -19,8 +19,8 @@ public class EratosthenesPrimeSieve {
      * @param args the command line arguments
      */
     
-    static int[] primeArray = null;
-    static int[] evenArray = null;
+    static int[] primeA = null;
+    static int[] evenA = null;
         
     static List<Integer> primes = new ArrayList<>();
     static List<Integer> evens = new ArrayList<>();
@@ -31,25 +31,61 @@ public class EratosthenesPrimeSieve {
         System.out.println("Obergrenze:");
         int ausw = Integer.parseInt(scanner.nextLine());
         EratosthenesPrimeSieve ep = new EratosthenesPrimeSieve(ausw);
-
+        
+        ep.primesList();
+        ep.evenList();
+        ep.printResults();
+        
     }
     
     public EratosthenesPrimeSieve(int grenze) {
-        primeArray = new int[grenze];
-        evenArray = new int[grenze];
+        primeA = new int[grenze];
+        evenA = new int[grenze];
 
     }
     
     public boolean isPrime(int p) {
+        if (p <= 1) {
+            return false;
+        }
+        if (p == 2) {
+            return true;
+        }
+
+        for (int i = 2; i < p; i++) {
+
+            if (p % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
     
     public void primesList() {
-    
+    for (int i = 0; i < primeA.length; i++) {
+            primeA[i] = i;
+        }
+        for (int i = 0; i < primeA.length; i++) {
+            if (isPrime(primeA[i]) == false) {
+                primeA[i] = -1;
+            }
+        }
+        for (int i = 0; i < primeA.length; i++) {
+            if (primeA[i] != -1) {
+                primes.add(primeA[i]);
+            }
+        }
     }
     
     public boolean isEven(int p) {
-        return true;
+        if (p <= 2) {
+            return false;
+        }
+
+        if (p % 2 == 0) {
+            return true;
+        }
+        return false;
     }
     
      public void evenList() {
