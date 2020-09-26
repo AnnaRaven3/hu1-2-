4156e370746719c98b525b0d672e663b7de136e2
AnnaRaven3 <Schmidinger.Anna@sus.htl-grieskirchen.at>
2020-09-26 18:36:15 +0200

    save 3

--- src/net/htl/grieskirchen/pos/aschmidinger18/erstehausuebung2/EratosthenesPrimeSieve.java
+++ src/net/htl/grieskirchen/pos/aschmidinger18/erstehausuebung2/EratosthenesPrimeSieve.java
@@ -5,6 +5,10 @@
  */
 package net.htl.grieskirchen.pos.aschmidinger18.erstehausuebung2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author annas
@@ -14,8 +18,46 @@ public class EratosthenesPrimeSieve {
     /**
      * @param args the command line arguments
      */
+    
+    static int[] primeArray = null;
+    static int[] evenArray = null;
+        
+    static List<Integer> primes = new ArrayList<>();
+    static List<Integer> evens = new ArrayList<>();
+    
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in, "Windows-1252");
+        System.out.println("Obergrenze:");
+        int ausw = Integer.parseInt(scanner.nextLine());
+        EratosthenesPrimeSieve ep = new EratosthenesPrimeSieve(ausw);
+
     }
+    
+    public EratosthenesPrimeSieve(int grenze) {
+        primeArray = new int[grenze];
+        evenArray = new int[grenze];
+
+    }
+    
+    public boolean isPrime(int p) {
+        return true;
+    }
+    
+    public void primesList() {
+    
+    }
+    
+    public boolean isEven(int p) {
+        return true;
+    }
+    
+     public void evenList() {
+         
+     }
+     
+     public void printResults() {
+         
+     }
     
 }
